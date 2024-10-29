a4ce199558abb2ac7dd2def96cb42a262069d6f2
JaneDoe <e.janedoe.87@gmail.com>
2024-10-29 14:15:57 +0300

    fxd

--- src/compare/ArraysComparator.java
+++ src/compare/ArraysComparator.java
@@ -2,8 +2,10 @@ package compare;
 
 public class ArraysComparator {
     public static  <T, U> boolean compare(T[] arr1, U[] arr2){
+        System.out.println(arr1.getClass());
         return arr1.length == arr2.length
-                && (arr1.getClass().isInstance(arr2.getClass())
-        || arr2.getClass().isInstance(arr1.getClass()));
+                && arr1.getClass().isInstance(arr2) || arr2.getClass().isInstance(arr1);
+
     }
+
 }
